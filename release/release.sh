9c175cc5b56617894740ea1a8c728569f266b3dd
Simon Kollberg <simonkollberg@gmail.com>
2020-09-23 14:01:13 +0200

    test4

--- release/release.sh
+++ release/release.sh
@@ -35,7 +35,7 @@ function check_version() {
     if git rev-parse "v${1}" >/dev/null 2>&1; then
         echo "v${1} tag already exists" >&2
         exit 1
-    else
+    fi
 }
 
 check_version "${new_version}"
